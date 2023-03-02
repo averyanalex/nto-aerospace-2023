d939ba66433af49ea43bbc51c9a94d25aa66d9a9
AveryanAlex <alex@averyan.ru>
2023-03-02 17:52:04 +0300

    Init capybara

---
+++ capybara/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "NTO Capybara";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustVersion
+          ];
+        };
+      }
+    );
+}
