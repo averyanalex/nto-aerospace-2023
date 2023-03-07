3a1358f2b933d951e3e9dabc36cc70a802c9d0e5
AveryanAlex <alex@averyan.ru>
2023-03-07 15:59:03 +0300

    Muskrat button handler

--- capybara/src/muskrat.rs
+++ capybara/src/muskrat.rs
@@ -1,13 +1,28 @@
 use anyhow::Result;
 // use log::*;
 use tokio::sync::{broadcast, mpsc};
+use tokio_serial::SerialPortBuilderExt;
 
 pub async fn run_muskrat(
-    mut set_angle_rx: mpsc::Receiver<f64>,
-    _button_tx: broadcast::Sender<()>,
+    mut _set_angle_rx: mpsc::Receiver<f64>,
+    button_tx: broadcast::Sender<()>,
 ) -> Result<()> {
-    while let Some(_angle) = set_angle_rx.recv().await {
-        // debug!("set angle = {}", angle);
+    let mut port = tokio_serial::new(
+        "/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4.1:1.0-port0",
+        115200,
+    )
+    .open_native_async()?;
+    port.set_exclusive(true)?;
+
+    loop {
+        port.readable().await?;
+        let mut buf = [0u8; 1];
+        port.try_read(&mut buf)?;
+        if buf[0] == 0x07 {
+            let _ = button_tx.send(());
+        }
     }
-    Ok(())
+    // while let Some(_angle) = set_angle_rx.recv().await {
+    //     // debug!("set angle = {}", angle);
+    // }
 }
