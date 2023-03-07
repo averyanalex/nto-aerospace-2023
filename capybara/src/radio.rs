7f3cb28a09138e22042b5b84e3f1fbe31a4862e2
AveryanAlex <alex@averyan.ru>
2023-03-07 20:09:21 +0300

    Radio config

--- capybara/src/radio.rs
+++ capybara/src/radio.rs
@@ -1,11 +1,76 @@
 use anyhow::Result;
 use log::*;
-use tokio::sync::broadcast;
+use tokio::time::{sleep, Duration};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    sync::broadcast,
+};
+use tokio_serial::{SerialPort, SerialPortBuilderExt};
+
+const RADIO_PORT: &str =
+    "/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-port0";
+const GET_CONFIG_CMD: [u8; 3] = [0xAA, 0xFA, 0x01];
+const SET_CONFIG_CMD: [u8; 18] = [
+    0xAA, 0xFA, 0x03, // set config command
+    7,    // RF Channel
+    1,    // 433 MHz RF Band
+    8,    // 115200 RF Rate
+    7,    // +20 dBm RF Power
+    8,    // 115200 Serial transmission rate
+    2,    // 8 bits data bits
+    1,    // 1 bits stop bits
+    1,    // no parity
+    0x22, 0xB4, 0xE6, 0x21, // NET ID
+    0x00, 0x00, // NODE ID
+    0x0A, // end of command
+];
 
 pub async fn run_radio(
     mut send_rx: broadcast::Receiver<Vec<u8>>,
     _receive_tx: broadcast::Sender<Vec<u8>>,
 ) -> Result<()> {
+    if true {
+        let mut port = tokio_serial::new(RADIO_PORT, 9600).open_native_async()?;
+        port.set_exclusive(true)?;
+        port.write_data_terminal_ready(true)?;
+
+        sleep(Duration::from_secs(3)).await;
+
+        while port.bytes_to_read()? > 0 {
+            warn!("reading trash byte");
+            port.read_exact(&mut [0u8]).await?;
+        }
+
+        port.write_all(&GET_CONFIG_CMD).await?;
+        let mut settings = [0u8; 16];
+        port.read_exact(&mut settings).await?;
+        info!("radio config: {:X?}", settings);
+
+        port.write_all(&SET_CONFIG_CMD).await?;
+        let mut res_buf = [0u8; 4];
+        port.read_exact(&mut res_buf).await?;
+        if &res_buf == b"OK\r\n" {
+            info!("set radio config");
+        } else {
+            error!("error setting radio config");
+        }
+        while port.bytes_to_read()? > 0 {
+            warn!("reading trash byte");
+            port.read_exact(&mut [0u8]).await?;
+        }
+    }
+
+    let mut port = tokio_serial::new(RADIO_PORT, 115200).open_native_async()?;
+    port.set_exclusive(true)?;
+    port.write_data_terminal_ready(false)?;
+
+    sleep(Duration::from_secs(3)).await;
+
+    while port.bytes_to_read()? > 0 {
+        warn!("reading trash byte");
+        port.read_exact(&mut [0u8]).await?;
+    }
+
     loop {
         let data_to_send = match send_rx.recv().await {
             Ok(d) => d,
