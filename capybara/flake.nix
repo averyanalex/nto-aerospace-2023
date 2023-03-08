cd5e070f6e7b5239ac3bec15f2bf59bdc2e44c39
AveryanAlex <alex@averyan.ru>
2023-03-09 01:49:49 +0300

    Cross compile

--- capybara/flake.nix
+++ capybara/flake.nix
@@ -13,48 +13,59 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ros.overlays.default ];
+        # pkgsNative = import nixpkgs { inherit system overlays; };
         pkgs = import nixpkgs {
           inherit system overlays;
-          # system = "aarch64-linux";
+          crossSystem = "aarch64-linux";
         };
-        rustVersion = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         inherit (import-cargo.builders) importCargo;
 
         buildInputs = with pkgs; [
+          stdenv.cc
+          # libgcc
+          # gcc
+          # binutils
+          # gcc
+
           libv4l
           dav1d
 
-          wayland
-          alsa-lib
-          udev
-          libxkbcommon
-          vulkan-loader
-          
+          # wayland
+          # alsa-lib
+          # udev
+          # libxkbcommon
+          # vulkan-loader
+
           zlib
 
           opencv
 
           # bevy x11
-          # xlibsWrapper
-          freetype
-          fontconfig
-          xorg.xorgproto
-          xorg.libX11
-          xorg.libXt
-          xorg.libXft
-          xorg.libXext
-          xorg.libSM
-          xorg.libICE
           # /xlibsWrapper
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
+          # freetype
+          # fontconfig
+          # xorg.xorgproto
+          # xorg.libX11
+          # xorg.libXt
+          # xorg.libXft
+          # xorg.libXext
+          # xorg.libSM
+          # xorg.libICE
+          # # /xlibsWrapper
+          # xorg.libXcursor
+          # xorg.libXrandr
+          # xorg.libXi
         ];
-        nativeBuildInputs = with pkgs; [
-          nasm
-          pkg-config
-          clang
-          mold
+
+        nativeBuildInputs = with pkgs.buildPackages; [
+          (rust-bin.fromRustupToolchainFile
+            ./rust-toolchain.toml)
+          
+          # glibc
+          # nasm
+          # pkg-config
+          # clang
+          # mold
         ];
 
         capybara = pkgs.stdenv.mkDerivation {
@@ -84,12 +95,10 @@
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            rustVersion
-          ] ++ buildInputs ++ nativeBuildInputs;
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          inherit buildInputs nativeBuildInputs;
+          # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
           # RUSTFLAGS = "-C target-cpu=cortex-a72";
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          # LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           ROSRUST_MSG_PATH = "${pkgs.rosPackages.noetic.std-msgs}/share/std_msgs:${pkgs.rosPackages.noetic.nav-msgs}/share/nav_msgs:${pkgs.rosPackages.noetic.geometry-msgs}/share/geometry_msgs";
         };
       }
