d73d18ecae6c1b792c02abdc2458befe9c9d4d5c
AveryanAlex <alex@averyan.ru>
2023-03-09 11:06:37 +0300

    Fix upload.sh

--- muskrat/upload.sh
+++ muskrat/upload.sh
@@ -1,1 +1,1 @@
-arduino-cli upload -b arduino:avr:mega -p /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4.1:1.0-port0
+arduino-cli compile -u -b arduino:avr:mega -p /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4.1:1.0-port0
