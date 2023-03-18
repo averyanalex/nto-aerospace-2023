7822ff1f07477dc53fd21fb059a36b7fcebb80f2
AveryanAlex <alex@averyan.ru>
2023-03-18 19:19:20 +0300

    Add ROS x11docker

---
+++ ros-x11docker/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+podman build -t docker.io/averyanalex/nto-ros-x11docker .
---
+++ ros-x11docker/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+x11docker --gpu --desktop docker.io/averyanalex/nto-ros-x11docker --backend=podman -X -m --sudouser -I
